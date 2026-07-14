a58f79748ee04e42cc652d7b090958a15ec5ef1d
Ubuntu <ubuntu@ip-172-31-42-182.ap-southeast-1.compute.internal>
2026-07-14 10:25:44 +0000

    changes in server code

--- auro-backend/auro/src/main/java/com/example/auro/config/DataSeeder.java
+++ auro-backend/auro/src/main/java/com/example/auro/config/DataSeeder.java
@@ -22,16 +22,6 @@ public class DataSeeder {
 
             create(authService, "Managing Director", "MD001", "md", "md@aurov.in", "9000000001", "Aurov Managing Director");
             create(authService, "Operational Head", "OH001", "oh", "operations@aurov.in", "9000000002", "Aurov Operational Head");
-            create(authService, "Sales Manager", "SM001", "salesmanager", "sales.manager@aurov.in", "9000000003", "Sales Manager");
-            create(authService, "Sales Executive", "SE001", "salesexecutive", "sales.executive@aurov.in", "9000000004", "Sales Executive");
-            create(authService, "Marketing Manager", "MM001", "marketingmanager", "marketing.manager@aurov.in", "9000000005", "Marketing Manager");
-            create(authService, "Marketing Executive", "ME001", "marketingexecutive", "marketing.executive@aurov.in", "9000000006", "Marketing Executive");
-            create(authService, "CRM Manager", "CM001", "crmmanager", "crm.manager@aurov.in", "9000000007", "CRM Manager");
-            create(authService, "CRM Executive", "CE001", "crmexecutive", "crm.executive@aurov.in", "9000000008", "CRM Executive");
-            create(authService, "Accounts Manager", "AM001", "accountsmanager", "accounts.manager@aurov.in", "9000000009", "Accounts Manager");
-            create(authService, "Accounts Executive", "AE001", "accountsexecutive", "accounts.executive@aurov.in", "9000000010", "Accounts Executive");
-            create(authService, "HR Manager", "HM001", "hrmanager", "hr.manager@aurov.in", "9000000011", "HR Manager");
-            create(authService, "HR Executive", "HE001", "hrexecutive", "hr.executive@aurov.in", "9000000012", "HR Executive");
         };
     }
 
